08dc0c6fcb5013431708629cb7737192483b6c52
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2022-12-29 23:30:57 +0100

    Moved default `SendAndDontWaitErrorHandler` implementations as inner classes in `SendAndDontWaitErrorHandler`

--- reactive/src/main/java/dk/cloudcreate/essentials/reactive/command/AbstractCommandBus.java
+++ reactive/src/main/java/dk/cloudcreate/essentials/reactive/command/AbstractCommandBus.java
@@ -1,7 +1,6 @@
 package dk.cloudcreate.essentials.reactive.command;
 
 import dk.cloudcreate.essentials.reactive.command.interceptor.*;
-import dk.cloudcreate.essentials.shared.Exceptions;
 import org.slf4j.*;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -26,7 +25,7 @@ public abstract class AbstractCommandBus implements CommandBus {
     protected final SendAndDontWaitErrorHandler sendAndDontWaitErrorHandler;
 
     protected AbstractCommandBus(List<CommandBusInterceptor> interceptors) {
-        this(new FallbackSendAndDontWaitErrorHandler(),
+        this(new SendAndDontWaitErrorHandler.FallbackSendAndDontWaitErrorHandler(),
              interceptors);
     }
 
@@ -140,42 +139,6 @@ public abstract class AbstractCommandBus implements CommandBus {
     @Override
     public boolean hasCommandHandler(CommandHandler commandHandler) {
         return commandHandlers.contains(requireNonNull(commandHandler, "No commandHandler provided"));
-    }
-
-    /**
-     * Fallback {@link SendAndDontWaitErrorHandler} that only error logs any issues.<br>
-     * Note: If the {@link FallbackSendAndDontWaitErrorHandler} is used with a Durable Command Bus (e.g. using DurableQueues),
-     * then any failing command will not be retried.<br>
-     * Instead use {@link RethrowingSendAndDontWaitErrorHandler}
-     */
-    public static class FallbackSendAndDontWaitErrorHandler implements SendAndDontWaitErrorHandler {
-        private static final Logger log = LoggerFactory.getLogger(FallbackSendAndDontWaitErrorHandler.class);
-
-        @Override
-        public void handleError(Exception exception, Object command, CommandHandler commandHandler) {
-            log.error(msg("SendAndDontWait ERROR: {} '{}' failed to handle command: {}",
-                          CommandHandler.class.getSimpleName(),
-                          commandHandler.getClass().getName(),
-                          command), exception);
-        }
-    }
-
-    /**
-     * Fallback {@link SendAndDontWaitErrorHandler} that error logs any issues and rethrows the exception.<br>
-     * The {@link RethrowingSendAndDontWaitErrorHandler} is compatible with a Durable Command Bus (e.g. using DurableQueues),
-     * as rethrowing the exceptions allows the command to be retried
-     */
-    public static class RethrowingSendAndDontWaitErrorHandler implements SendAndDontWaitErrorHandler {
-        private static final Logger log = LoggerFactory.getLogger(FallbackSendAndDontWaitErrorHandler.class);
-
-        @Override
-        public void handleError(Exception exception, Object command, CommandHandler commandHandler) {
-            log.error(msg("SendAndDontWait ERROR: {} '{}' failed to handle command: {}",
-                          CommandHandler.class.getSimpleName(),
-                          commandHandler.getClass().getName(),
-                          command), exception);
-            Exceptions.sneakyThrow(exception);
-        }
     }
 
     @Override
--- reactive/src/main/java/dk/cloudcreate/essentials/reactive/command/SendAndDontWaitErrorHandler.java
+++ reactive/src/main/java/dk/cloudcreate/essentials/reactive/command/SendAndDontWaitErrorHandler.java
@@ -1,6 +1,11 @@
 package dk.cloudcreate.essentials.reactive.command;
 
+import dk.cloudcreate.essentials.shared.Exceptions;
+import org.slf4j.*;
+
 import java.time.Duration;
+
+import static dk.cloudcreate.essentials.shared.MessageFormatter.msg;
 
 /**
  * Exception handler that will handle errors that occur during {@link CommandBus#sendAndDontWait(Object)}/{@link CommandBus#sendAndDontWait(Object, Duration)}
@@ -14,4 +19,40 @@ public interface SendAndDontWaitErrorHandler {
      * @param commandHandler the command handler that can handle the command
      */
     void handleError(Exception exception, Object command, CommandHandler commandHandler);
+
+    /**
+     * Fallback {@link SendAndDontWaitErrorHandler} that only error logs any issues.<br>
+     * Note: If the {@link FallbackSendAndDontWaitErrorHandler} is used with a Durable Command Bus (e.g. using DurableQueues),
+     * then any failing command will not be retried.<br>
+     * Instead use {@link RethrowingSendAndDontWaitErrorHandler}
+     */
+    class FallbackSendAndDontWaitErrorHandler implements SendAndDontWaitErrorHandler {
+        private static final Logger log = LoggerFactory.getLogger(FallbackSendAndDontWaitErrorHandler.class);
+
+        @Override
+        public void handleError(Exception exception, Object command, CommandHandler commandHandler) {
+            log.error(msg("SendAndDontWait ERROR: {} '{}' failed to handle command: {}",
+                          CommandHandler.class.getSimpleName(),
+                          commandHandler.getClass().getName(),
+                          command), exception);
+        }
+    }
+
+    /**
+     * Fallback {@link SendAndDontWaitErrorHandler} that error logs any issues and rethrows the exception.<br>
+     * The {@link RethrowingSendAndDontWaitErrorHandler} is compatible with a Durable Command Bus (e.g. using DurableQueues),
+     * as rethrowing the exceptions allows the command to be retried
+     */
+    class RethrowingSendAndDontWaitErrorHandler implements SendAndDontWaitErrorHandler {
+        private static final Logger log = LoggerFactory.getLogger(FallbackSendAndDontWaitErrorHandler.class);
+
+        @Override
+        public void handleError(Exception exception, Object command, CommandHandler commandHandler) {
+            log.error(msg("SendAndDontWait ERROR: {} '{}' failed to handle command: {}",
+                          CommandHandler.class.getSimpleName(),
+                          commandHandler.getClass().getName(),
+                          command), exception);
+            Exceptions.sneakyThrow(exception);
+        }
+    }
 }
