8bf7266198772beef5fa3eee12ef81b46fb5af08
cloudcreate.dk <96371136+cloudcreate-dk@users.noreply.github.com>
2023-12-17 19:40:11 +0100

    Added license reference to DeciderTest.java

--- components/eventsourced-aggregates/src/test/java/dk/cloudcreate/essentials/components/eventsourced/aggregates/decider/DeciderTest.java
+++ components/eventsourced-aggregates/src/test/java/dk/cloudcreate/essentials/components/eventsourced/aggregates/decider/DeciderTest.java
@@ -42,6 +42,37 @@ import static dk.cloudcreate.essentials.shared.FailFast.*;
  * Successfully guessing the secret before reaching the limit results in {@link GuessingGameEvent.GameWon},
  * while exceeding the allowed guesses leads to {@link GuessingGameEvent.GameLost}.
  *
+ * <p>
+ * Note:<br>
+ * <pre>
+ * This example/test is inspired by this <a href="https://dev.to/jakub_zalas/functional-event-sourcing-example-in-kotlin-3245">article</a>.<br>
+ * The example in the referenced article and its code <a href="https://github.com/jakzal/mastermind/tree/main">MasterMind</a>
+ * are released under this <a href="https://github.com/jakzal/mastermind/blob/main/LICENSE">MIT license</a>:
+ *
+ * Copyright (c) 2015 Jakub Zalas <jakub@zalas.pl>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * </pre>
+ *
  * @see DeciderBasedCommandHandlerIT
  */
 class DeciderTest {
