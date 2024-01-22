882b910ffee9b4c611c7732b1deebb95f765de1c
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2024-01-22 08:41:13 +0100

    Introduced the concept of `InterceptorOrder` to control in which order an Interceptor is invoked
    (cherry picked from commit ed1662e06666fff27919bb4ed8774a0962096095)

--- shared/src/main/java/dk/cloudcreate/essentials/shared/interceptor/DefaultInterceptorChain.java
+++ shared/src/main/java/dk/cloudcreate/essentials/shared/interceptor/DefaultInterceptorChain.java
@@ -42,7 +42,7 @@ public class DefaultInterceptorChain<OPERATION, RESULT, INTERCEPTOR_TYPE extends
      * Create a new {@link InterceptorChain} instance for the provided <code>operation</code>
      *
      * @param operation                the operation to intercept, aka. the argument to the interceptor
-     * @param interceptors             the {@link Interceptor}'s (can be an empty List if no interceptors have been configured)
+     * @param interceptors             the ordered {@link Interceptor}'s (can be an empty List if no interceptors have been configured)
      * @param interceptorMethodInvoker the function that's responsible for invoking the matching {@link Interceptor} method
      * @param defaultBehaviour         the default behaviour for the given <code>operation</code> in case none of the interceptors provided a different result and stopped the interceptor chain
      */
@@ -54,6 +54,21 @@ public class DefaultInterceptorChain<OPERATION, RESULT, INTERCEPTOR_TYPE extends
         this.interceptorIterator = requireNonNull(interceptors, "No interceptors provided").iterator();
         this.interceptorMethodInvoker = requireNonNull(interceptorMethodInvoker, "No interceptorMethodInvoker provided");
         this.defaultBehaviour = requireNonNull(defaultBehaviour, "No defaultBehaviour supplier provided");
+    }
+
+    /**
+     * Order the interceptors according to the specified {@link InterceptorOrder} annotation (if the annotation is left out
+     * then the default order is 10)
+     *
+     * @param interceptors       the list of interceptors that should be ordered (expects a mutable list that can be reordered)
+     * @param <INTERCEPTOR_TYPE> the type of interceptor contained in the list
+     * @return the <code>interceptors</code> parameter after it has been reordered
+     */
+    public static <INTERCEPTOR_TYPE> List<INTERCEPTOR_TYPE> sortInterceptorsByOrder(List<INTERCEPTOR_TYPE> interceptors) {
+        interceptors.sort(Comparator.comparingInt(interceptor ->
+                                                          interceptor.getClass().getAnnotation(InterceptorOrder.class) != null ?
+                                                          interceptor.getClass().getAnnotation(InterceptorOrder.class).value() : 10));
+        return interceptors;
     }
 
     @Override
