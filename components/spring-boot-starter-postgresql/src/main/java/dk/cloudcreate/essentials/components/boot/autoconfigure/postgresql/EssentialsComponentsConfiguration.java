45d0f98046e80393b81bd97e64e8643a7589f397
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2024-01-22 08:49:29 +0100

    Changed tracing/metric related Bean methods in `EssentialsComponentsConfiguration` to use `@ConditionalOnProperty(prefix = "management.tracing", name = "enabled", havingValue = "true")` instead of `@ConditionalOnEnabledTracing`. Also made all tracing/metric arguments `Optional`
    Added `additionalModules` parameter to `essentialComponentsObjectMapper` method in `EssentialsComponentsConfiguration` to include additional Jackson `Module`'s configured elsewhere
    
    Added support for `JdbiConfigurationCallback`

--- components/spring-boot-starter-postgresql/src/main/java/dk/cloudcreate/essentials/components/boot/autoconfigure/postgresql/EssentialsComponentsConfiguration.java
+++ components/spring-boot-starter-postgresql/src/main/java/dk/cloudcreate/essentials/components/boot/autoconfigure/postgresql/EssentialsComponentsConfiguration.java
@@ -18,6 +18,7 @@ package dk.cloudcreate.essentials.components.boot.autoconfigure.postgresql;
 
 
 import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -50,7 +51,6 @@ import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.postgres.PostgresPlugin;
 import org.slf4j.*;
 import org.springframework.beans.BeansException;
-import org.springframework.boot.actuate.autoconfigure.tracing.ConditionalOnEnabledTracing;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.*;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -81,21 +81,21 @@ public class EssentialsComponentsConfiguration implements ApplicationListener<Ap
     }
 
     @Bean
-    @ConditionalOnEnabledTracing
-    public DurableQueuesMicrometerTracingInterceptor durableQueuesMicrometerTracingInterceptor(Tracer tracer,
-                                                                                               Propagator propagator,
-                                                                                               ObservationRegistry observationRegistry,
+    @ConditionalOnProperty(prefix = "management.tracing", name = "enabled", havingValue = "true")
+    public DurableQueuesMicrometerTracingInterceptor durableQueuesMicrometerTracingInterceptor(Optional<Tracer> tracer,
+                                                                                               Optional<Propagator> propagator,
+                                                                                               Optional<ObservationRegistry> observationRegistry,
                                                                                                EssentialsComponentsProperties properties) {
-        return new DurableQueuesMicrometerTracingInterceptor(tracer,
-                                                             propagator,
-                                                             observationRegistry,
+        return new DurableQueuesMicrometerTracingInterceptor(tracer.get(),
+                                                             propagator.get(),
+                                                             observationRegistry.get(),
                                                              properties.getDurableQueues().isVerboseTracing());
     }
 
     @Bean
-    @ConditionalOnEnabledTracing
-    public DurableQueuesMicrometerInterceptor durableQueuesMicrometerInterceptor(MeterRegistry meterRegistry) {
-        return new DurableQueuesMicrometerInterceptor(meterRegistry);
+    @ConditionalOnProperty(prefix = "management.tracing", name = "enabled", havingValue = "true")
+    public DurableQueuesMicrometerInterceptor durableQueuesMicrometerInterceptor(Optional<MeterRegistry> meterRegistry) {
+        return new DurableQueuesMicrometerInterceptor(meterRegistry.get());
     }
 
     /**
@@ -320,11 +320,13 @@ public class EssentialsComponentsConfiguration implements ApplicationListener<Ap
      * {@link ObjectMapper} responsible for serializing/deserializing the raw Java events to and from JSON
      *
      * @param optionalEssentialsImmutableJacksonModule the optional {@link EssentialsImmutableJacksonModule}
+     * @param additionalModules additional {@link Module}'s found in the {@link ApplicationContext}
      * @return the {@link ObjectMapper} responsible for serializing/deserializing the raw Java events to and from JSON
      */
     @Bean
     @ConditionalOnMissingBean
-    public ObjectMapper essentialComponentsObjectMapper(Optional<EssentialsImmutableJacksonModule> optionalEssentialsImmutableJacksonModule) {
+    public ObjectMapper essentialComponentsObjectMapper(Optional<EssentialsImmutableJacksonModule> optionalEssentialsImmutableJacksonModule,
+                                                        List<Module> additionalModules) {
         var objectMapperBuilder = JsonMapper.builder()
                                             .disable(MapperFeature.AUTO_DETECT_GETTERS)
                                             .disable(MapperFeature.AUTO_DETECT_IS_GETTERS)
@@ -339,6 +341,8 @@ public class EssentialsComponentsConfiguration implements ApplicationListener<Ap
                                             .addModule(new Jdk8Module())
                                             .addModule(new JavaTimeModule())
                                             .addModule(new EssentialTypesJacksonModule());
+
+        additionalModules.forEach(objectMapperBuilder::addModule);
 
         optionalEssentialsImmutableJacksonModule.ifPresent(essentialsImmutableJacksonModule -> {
             objectMapperBuilder.addModule(new EssentialsImmutableJacksonModule());
@@ -370,6 +374,17 @@ public class EssentialsComponentsConfiguration implements ApplicationListener<Ap
                     lifecycleBean.start();
                 }
             });
+
+            var callbacks = applicationContext.getBeansOfType(JdbiConfigurationCallback.class).values();
+            if (!callbacks.isEmpty()) {
+                var jdbi = applicationContext.getBean(Jdbi.class);
+                callbacks.forEach(configureJdbiCallback -> {
+                    log.info("Calling {}: {}",
+                             JdbiConfigurationCallback.class.getSimpleName(),
+                             configureJdbiCallback.getClass().getName());
+                    configureJdbiCallback.configure(jdbi);
+                });
+            }
         } else if (event instanceof ContextClosedEvent) {
             log.info("{} - has Context already been closed: {}", event.getClass().getSimpleName(), closed);
             if (!closed) {
