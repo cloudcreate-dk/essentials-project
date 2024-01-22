e37755fa9cd459581dd298085e22325fe62853bf
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2024-01-22 08:45:17 +0100

    Added `InterceptorOrder` support to `PostgresqlEventStore` and added `UnableToExecuteStatementException` to the list of transient errors
    (cherry picked from commit cf00cf1162533a4860d77d8626378afb443eec5c)

--- components/postgresql-event-store/src/main/java/dk/cloudcreate/essentials/components/eventsourced/eventstore/postgresql/interceptor/EventStoreInterceptorChain.java
+++ components/postgresql-event-store/src/main/java/dk/cloudcreate/essentials/components/eventsourced/eventstore/postgresql/interceptor/EventStoreInterceptorChain.java
@@ -84,7 +84,7 @@ public interface EventStoreInterceptorChain<OPERATION, RESULT> {
          * Create a new {@link EventStoreInterceptorChain} instance for the provided <code>operation</code>
          *
          * @param operation                  the {@link EventStore} operation to intercept, aka. the argument to the interceptor
-         * @param interceptors               the {@link EventStoreInterceptor}'s (can be an empty List if no interceptors have been configured)
+         * @param interceptors               the ordered {@link EventStoreInterceptor}'s (can be an empty List if no interceptors have been configured)
          * @param interceptorMethodInvoker   the function that's responsible for invoking the matching {@link EventStoreInterceptor} method
          * @param defaultEventStoreBehaviour the default {@link EventStore} behaviour for the given <code>operation</code> in case none of the interceptors provided a different result and stopped the interceptor chain
          */
