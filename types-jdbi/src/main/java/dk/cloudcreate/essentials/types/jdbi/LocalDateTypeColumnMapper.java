86075a430d785c48ebab7c85a2083ea17605173a
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2024-02-04 23:07:54 +0100

    Renamed `LocalDateColumnMapper` to `LocalDateTypeColumnMapper`

--- types-jdbi/src/main/java/dk/cloudcreate/essentials/types/jdbi/LocalDateTypeColumnMapper.java
+++ types-jdbi/src/main/java/dk/cloudcreate/essentials/types/jdbi/LocalDateTypeColumnMapper.java
@@ -35,16 +35,16 @@ import static dk.cloudcreate.essentials.shared.FailFast.requireNonNull;
  *
  * @param <T> the concrete {@link LocalDateType} this instance is mapping
  */
-public abstract class LocalDateColumnMapper<T extends LocalDateType<T>> implements ColumnMapper<T> {
+public abstract class LocalDateTypeColumnMapper<T extends LocalDateType<T>> implements ColumnMapper<T> {
     private final Class<T> concreteType;
 
     @SuppressWarnings("unchecked")
-    public LocalDateColumnMapper() {
+    public LocalDateTypeColumnMapper() {
         concreteType = (Class<T>) GenericType.resolveGenericTypeOnSuperClass(this.getClass(),
                                                                              0);
     }
 
-    public LocalDateColumnMapper(Class<T> concreteType) {
+    public LocalDateTypeColumnMapper(Class<T> concreteType) {
         this.concreteType = requireNonNull(concreteType, "No concreteType provided");
     }
 
