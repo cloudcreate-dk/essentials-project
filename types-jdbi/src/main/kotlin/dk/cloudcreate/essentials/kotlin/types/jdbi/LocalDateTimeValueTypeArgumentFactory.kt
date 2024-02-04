a26bb2a80f6dd80e73336ad8791fff0a9ada20ae
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2024-02-04 23:12:37 +0100

    Moved `ArgumentFactory`/`ColumnMapper` for the kotlin value types to a kotlin sub package

---
+++ types-jdbi/src/main/kotlin/dk/cloudcreate/essentials/kotlin/types/jdbi/LocalDateTimeValueTypeArgumentFactory.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dk.cloudcreate.essentials.kotlin.types.jdbi
+
+import dk.cloudcreate.essentials.kotlin.types.LocalDateTimeValueType
+import org.jdbi.v3.core.argument.AbstractArgumentFactory
+import org.jdbi.v3.core.argument.Argument
+import org.jdbi.v3.core.config.ConfigRegistry
+import org.jdbi.v3.core.statement.StatementContext
+import java.sql.PreparedStatement
+import java.sql.Timestamp
+import java.sql.Types
+
+/**
+ * Base implementation for a value class that implements [LocalDateTimeValueType]<br>
+ *
+ * @param <T> the concrete [LocalDateTimeValueType] value class
+ */
+abstract class LocalDateTimeValueTypeArgumentFactory<T : LocalDateTimeValueType> : AbstractArgumentFactory<T>(Types.TIMESTAMP) {
+    override fun build(value: T, config: ConfigRegistry?): Argument {
+        return Argument { position: Int, statement: PreparedStatement, ctx: StatementContext? ->
+            statement.setTimestamp(
+                position,
+                Timestamp.valueOf(value.value)
+            )
+        }
+    }
+}
