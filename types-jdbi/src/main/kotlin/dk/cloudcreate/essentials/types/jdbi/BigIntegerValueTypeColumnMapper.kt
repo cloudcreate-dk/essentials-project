ab21dea2e613a770513ed44865bf10ff8aeecfb4
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2024-02-04 23:09:21 +0100

    Added `OffsetDateTimeValueType` Added `ArgumentFactory`/`ColumnMapper` for the kotlin value types (tests are still missing)

---
+++ types-jdbi/src/main/kotlin/dk/cloudcreate/essentials/types/jdbi/BigIntegerValueTypeColumnMapper.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dk.cloudcreate.essentials.types.jdbi
+
+import dk.cloudcreate.essentials.kotlin.types.BigIntegerValueType
+import dk.cloudcreate.essentials.shared.types.GenericType
+import org.jdbi.v3.core.mapper.ColumnMapper
+import org.jdbi.v3.core.statement.StatementContext
+import java.math.BigInteger
+import java.sql.ResultSet
+import java.sql.SQLException
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
+
+/**
+ * Generic [ColumnMapper] for [BigIntegerValueType]'s
+ *
+ * @param <T> the concrete [BigIntegerValueType] this instance is mapping
+ */
+abstract class BigIntegerValueTypeColumnMapper<T : BigIntegerValueType> : ColumnMapper<T?> {
+    private val concreteType: KClass<T>
+
+    constructor() {
+        concreteType = (GenericType.resolveGenericTypeOnSuperClass(
+            this.javaClass,
+            0
+        ) as Class<T>).kotlin
+    }
+
+    constructor(concreteType: KClass<T>) {
+        this.concreteType = concreteType
+    }
+
+    @Throws(SQLException::class)
+    override fun map(r: ResultSet, columnNumber: Int, ctx: StatementContext): T? {
+        val value = r.getBytes(columnNumber)
+        return if (value == null) null else concreteType.primaryConstructor!!.call(BigInteger(value))
+    }
+}
---
+++ types-jdbi/src/main/kotlin/dk/cloudcreate/essentials/types/jdbi/LongValueTypeArgumentFactory.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dk.cloudcreate.essentials.types.jdbi
+
+import dk.cloudcreate.essentials.kotlin.types.LongValueType
+import org.jdbi.v3.core.argument.AbstractArgumentFactory
+import org.jdbi.v3.core.argument.Argument
+import org.jdbi.v3.core.config.ConfigRegistry
+import org.jdbi.v3.core.statement.StatementContext
+import java.sql.PreparedStatement
+import java.sql.Types
+
+/**
+ * Base implementation for a value class that implements [LongValueType]<br>
+ *
+ * @param <T> the concrete [LongValueType] value class
+ */
+abstract class LongValueTypeArgumentFactory<T : LongValueType> : AbstractArgumentFactory<T>(Types.BIGINT) {
+    override fun build(value: T, config: ConfigRegistry?): Argument {
+        return Argument { position: Int, statement: PreparedStatement, ctx: StatementContext? ->
+            statement.setLong(
+                position,
+                value.value
+            )
+        }
+    }
+}
