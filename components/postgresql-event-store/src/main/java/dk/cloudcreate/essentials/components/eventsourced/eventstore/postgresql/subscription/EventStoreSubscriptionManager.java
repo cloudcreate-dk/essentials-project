59691cd517ac9cab74fe40da1a1b39750e821b4f
Anders Helmar <anders.helmar@gmail.com>
2024-06-21 11:04:54 +0200

    only report subscriber lag on active and async subscriptions

--- components/postgresql-event-store/src/main/java/dk/cloudcreate/essentials/components/eventsourced/eventstore/postgresql/subscription/EventStoreSubscriptionManager.java
+++ components/postgresql-event-store/src/main/java/dk/cloudcreate/essentials/components/eventsourced/eventstore/postgresql/subscription/EventStoreSubscriptionManager.java
@@ -16,29 +16,44 @@
 
 package dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.subscription;
 
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 import dk.cloudcreate.essentials.components.distributed.fencedlock.postgresql.PostgresqlFencedLockManager;
-import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.*;
-import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.bus.*;
-import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.eventstream.*;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.EventStore;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.EventStoreException;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.EventStoreSubscription;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.bus.CommitStage;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.bus.PersistedEvents;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.eventstream.AggregateType;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.eventstream.PersistedEvent;
 import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.serializer.json.EventJSON;
 import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.types.GlobalEventOrder;
 import dk.cloudcreate.essentials.components.foundation.Lifecycle;
-import dk.cloudcreate.essentials.components.foundation.fencedlock.*;
+import dk.cloudcreate.essentials.components.foundation.fencedlock.FencedLock;
+import dk.cloudcreate.essentials.components.foundation.fencedlock.FencedLockManager;
+import dk.cloudcreate.essentials.components.foundation.fencedlock.LockCallback;
+import dk.cloudcreate.essentials.components.foundation.fencedlock.LockName;
 import dk.cloudcreate.essentials.components.foundation.messaging.eip.store_and_forward.Inbox;
 import dk.cloudcreate.essentials.components.foundation.messaging.queue.OrderedMessage;
 import dk.cloudcreate.essentials.components.foundation.transaction.UnitOfWork;
-import dk.cloudcreate.essentials.components.foundation.types.*;
+import dk.cloudcreate.essentials.components.foundation.types.SubscriberId;
+import dk.cloudcreate.essentials.components.foundation.types.Tenant;
 import dk.cloudcreate.essentials.shared.FailFast;
 import dk.cloudcreate.essentials.shared.concurrent.ThreadFactoryBuilder;
 import dk.cloudcreate.essentials.shared.functional.tuple.Pair;
 import org.reactivestreams.Subscription;
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.BaseSubscriber;
-
-import java.time.Duration;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.stream.Collectors;
 
 import static dk.cloudcreate.essentials.shared.FailFast.requireNonNull;
 import static dk.cloudcreate.essentials.shared.MessageFormatter.msg;
@@ -470,7 +485,10 @@ public interface EventStoreSubscriptionManager extends Lifecycle {
 
     Set<Pair<SubscriberId, AggregateType>> getActiveSubscriptions();
 
-    GlobalEventOrder getCurrentEventOrder(SubscriberId subscriberId, AggregateType aggregateType);
+    /**
+     * @return current event order for the given subscriber only of the subscriber has a registered resume point
+     */
+    Optional<GlobalEventOrder> getCurrentEventOrder(SubscriberId subscriberId, AggregateType aggregateType);
 
     /**
      * Default implementation of the {@link EventStoreSubscriptionManager} interface
@@ -576,15 +594,17 @@ public interface EventStoreSubscriptionManager extends Lifecycle {
 
         @Override
         public Set<Pair<SubscriberId, AggregateType>> getActiveSubscriptions() {
-            return this.subscribers.keySet();
+            return this.subscribers.entrySet().stream()
+                .filter(e -> e.getValue().isActive())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
         }
 
         @Override
-        public GlobalEventOrder getCurrentEventOrder(SubscriberId subscriberId, AggregateType aggregateType) {
+        public Optional<GlobalEventOrder> getCurrentEventOrder(SubscriberId subscriberId, AggregateType aggregateType) {
             return Optional.ofNullable(this.subscribers.get(Pair.of(subscriberId, aggregateType)))
                            .flatMap(EventStoreSubscription::currentResumePoint)
-                           .map(SubscriptionResumePoint::getResumeFromAndIncluding)
-                           .orElse(GlobalEventOrder.FIRST_GLOBAL_EVENT_ORDER);
+                           .map(SubscriptionResumePoint::getResumeFromAndIncluding);
         }
 
         private void saveResumePointsForAllSubscribers() {
