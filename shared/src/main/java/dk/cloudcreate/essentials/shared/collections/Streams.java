63035577fe5a63b216bdff5c2e387fdd47c2d240
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2025-02-05 09:09:53 +0100

    - Improved `shared` module documentation

--- shared/src/main/java/dk/cloudcreate/essentials/shared/collections/Streams.java
+++ shared/src/main/java/dk/cloudcreate/essentials/shared/collections/Streams.java
@@ -23,7 +23,43 @@ import java.util.stream.*;
 import static dk.cloudcreate.essentials.shared.FailFast.requireNonNull;
 import static dk.cloudcreate.essentials.shared.MessageFormatter.msg;
 
+/**
+ * Utility class for working with streams.
+ */
 public final class Streams {
+    /**
+     * Combines two streams into a single stream by applying a zip function to pairs of elements from the two streams.
+     * The streams must have equal sizes. The resulting stream will have the same order as the input streams.
+     *
+     * <p>The zip operation combines elements of the two streams pairwise: the first element from {@code streamT} with the
+     * first element from {@code streamU}, the second element with the second, and so on. The provided {@code zipFunction}
+     * determines how the elements are combined into the resulting stream.
+     *
+     * <p>The resulting stream will automatically close both input streams when it is closed.
+     *
+     * <p>Example:
+     * <pre>{@code
+     * var tStream = Stream.of("A", "B", "C", "D", "E");
+     * var uStream = Stream.of("1", "2", "3", "4", "5");
+     *
+     * List<String> result = Streams.zipOrderedAndEqualSizedStreams(tStream, uStream, (t, u) -> t + ":" + u)
+     *                         .collect(Collectors.toList());
+     *
+     * assertThat(result).isEqualTo(List.of("A:1", "B:2", "C:3", "D:4", "E:5"));
+     * }
+     * </pre>
+     *
+     * @param <R>         The type of elements in the resulting stream.
+     * @param <T>         The type of elements in the first input stream.
+     * @param <U>         The type of elements in the second input stream.
+     * @param streamT     the first input stream; must be non-null.
+     * @param streamU     the second input stream; must be non-null.
+     * @param zipFunction a function to combine elements from {@code streamT} and {@code streamU}; must be non-null.
+     * @return a new stream consisting of elements resulting from applying the {@code zipFunction} to pairs of elements
+     * from {@code streamT} and {@code streamU}.
+     * @throws IllegalArgumentException if {@code streamT}, {@code streamU}, or {@code zipFunction} is null or
+     *                                  if {@code streamT} or {@code streamU} do not have equal sizes.
+     */
     public static <R, T, U> Stream<R> zipOrderedAndEqualSizedStreams(Stream<T> streamT, Stream<U> streamU, BiFunction<T, U, R> zipFunction) {
         requireNonNull(streamT, "No streamT provided");
         requireNonNull(streamU, "No streamU provided");
